f5a4a196799657cae88d60d67a77541e366f74f4
Alexey Pozhalov <pozhalov7@mail.ru>
2023-09-01 19:58:01 +0300

    test was changed

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,10 +1,10 @@
 import calcHealthPoint from "../basic";
 
 test.each([
-  ['мечник', 10],
-  ['маг', 100],
-  ['лучник', 80]
-])('testing function with health point character with %c character %h health',(character,healthPoint,expected) => {
+  ['мечник', 10, 'critical'],
+  ['маг', 100, 'healthy'],
+  ['лучник', 80, 'healthy']
+])('testing function with health point character with %c character %h health', (characters, healthPoint, expected) => {
   const result = calcHealthPoint(healthPoint)
 
   expect(result).toBe(expected);
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,8 +1,8 @@
-const character = [
+const characters = [
   {name: 'мечник', health: 10},
   {name: 'маг', health: 100},
   {name: 'лучник', health: 80},
 ]
-export default function calcHealthPoint(...character) {
-  return character.name && character.health >= 50 ? 'healthy' : character.health > 15 && character.health < 50 ? 'wounded' : 'critical';
+export default function calcHealthPoint({...heroes}) {
+  return heroes.name && heroes.health >= 50 ? 'healthy' : heroes.health > 15 && heroes.health < 50 ? 'wounded' : 'critical';
 }
